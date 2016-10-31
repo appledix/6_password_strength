7ff48903e4750cf494b73d2fa37f1cb21ba80583
appledix <dxl2010@mail.ru>
2016-10-31 14:25:05 +0300

    Внесены изменения.

--- password_strength.py
+++ password_strength.py
@@ -1,3 +1,4 @@
+import math
 import getpass
 import re
 
@@ -22,52 +23,58 @@ def load_blacklist(filepath):
         data = text_file.read()
     return re.findall(r'\w+', data)
 
-def get_blacklist_filepath():
-    return input("Введите адрес текстового файла " +
-        " cо списком запрещённых паролей\n(можно оставить пустым):\n")
-
-def appears_in_blacklist(password):
-    while(True):
-        try:
-            blacklist_filepath = get_blacklist_filepath()
-            if blacklist_filepath:
-                return password in load_blacklist(blacklist_filepath)
-        except Exception:
-            continue
-        else:
-            break
-    return False
+def appears_in_blacklist(password, blacklist):
+    return password in blacklist
 
 def get_length_points(password):
     password_length = len(password)
-    points = 0 if password_length < MINIMUM_PASSWORD_LENGTH \
-    else round((password_length - MINIMUM_PASSWORD_LENGTH) / 4 + 0.5)
-    return points
+    if password_length < MINIMUM_PASSWORD_LENGTH:
+        return 0
+    else:
+        return math.ceil((password_length - MINIMUM_PASSWORD_LENGTH) / 4)
 
-def calculate_password_strength(password):
+def calculate_password_strength(password, blacklist=None):
     password_strength = 1
 
-    if len(password) < MINIMUM_PASSWORD_LENGTH: return password_strength
+    if len(password) < MINIMUM_PASSWORD_LENGTH: 
+        return password_strength
     
-    if appears_in_blacklist(password): return password_strength
+    if blacklist:
+        if appears_in_blacklist(password, blacklist): 
+            return password_strength
 
-    if includes_letters(password) \
-    and includes_digits(password): password_strength += 2
+    if includes_letters(password) and includes_digits(password): 
+        password_strength += 2
 
-    if case_sensitive(password): password_strength += 2
+    if case_sensitive(password): 
+        password_strength += 2
 
-    if includes_special_chars(password): password_strength += 1
+    if includes_special_chars(password): 
+        password_strength += 1
 
     password_strength += get_length_points(password)
     
-    if password_strength > 10: password_strength = 10
+    if password_strength > 10: 
+        password_strength = 10
 
     return password_strength
 
 
 if __name__ == '__main__':
     while True:
-        password = getpass.getpass(prompt="Введите пароль для проверки:\n", stream=None)
-        if password: break
-    password_strength = calculate_password_strength(password)
+        password = \
+        getpass.getpass(prompt="Введите пароль для проверки:\n", stream=None)
+        if password: 
+            break
+
+    blacklist_filepath = input("Введите адрес текстового файла " +
+        " cо списком запрещённых паролей\n(можно оставить пустым):\n")
+    blacklist = None
+    if blacklist_filepath:
+        try:
+            blacklist = load_blacklist(blacklist_filepath)
+        except FileNotFoundError:
+            print("Не удалось найти файл по введенному адресу.")
+
+    password_strength = calculate_password_strength(password, blacklist)
     print("Оценка пароля:%d/10." % password_strength)
